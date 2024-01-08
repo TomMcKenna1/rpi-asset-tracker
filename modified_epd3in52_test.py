10e3c99668a97d1e2e02aa51426fdfa1d89b1e3f
Tom McKenna <tom2mckenna@gmail.com>
2024-01-08 12:47:09 +0000

    remove bitmap demo

--- modified_epd3in52_test.py
+++ modified_epd3in52_test.py
@@ -52,21 +52,21 @@ try:
     epd.refresh()
     time.sleep(2)
     
-    logging.info("3.read bmp file")
-    Himage = Image.open(os.path.join(picdir, '3in52-1.bmp'))
-    epd.display(epd.getbuffer(Himage.transpose(Image.FLIP_TOP_BOTTOM)))
-    epd.lut_GC()
-    epd.refresh()
-    time.sleep(2)
+    # logging.info("3.read bmp file")
+    # Himage = Image.open(os.path.join(picdir, '3in52-1.bmp'))
+    # epd.display(epd.getbuffer(Himage.transpose(Image.FLIP_TOP_BOTTOM)))
+    # epd.lut_GC()
+    # epd.refresh()
+    # time.sleep(2)
     
-    logging.info("4.read bmp file on window")
-    Himage2 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-    Himage2.paste(bmp, (50,10))
-    epd.display(epd.getbuffer(Himage2.transpose(Image.FLIP_TOP_BOTTOM)))
-    epd.lut_GC()
-    epd.refresh()
-    time.sleep(2)
+    # logging.info("4.read bmp file on window")
+    # Himage2 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    # bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
+    # Himage2.paste(bmp, (50,10))
+    # epd.display(epd.getbuffer(Himage2.transpose(Image.FLIP_TOP_BOTTOM)))
+    # epd.lut_GC()
+    # epd.refresh()
+    # time.sleep(2)
 
 
     # print("Quick refresh is supported, but the refresh effect is not good, but it is not recommended")
