a820d64ffeff4714ff1969e065e037d46ca596be
Tom McKenna <tom2mckenna@gmail.com>
2024-02-14 13:27:27 +0000

    fix: setup shell script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get autoremove -y bluez
 sudo apt-get install -y build-essential libreadline-dev libical-dev libdbus-1-dev libudev-dev libglib2.0-dev python3-docutils python3-pip python3-dev python3-pandas python3-pil python3-numpy
 wget -P ~/bluez http://www.kernel.org/pub/linux/bluetooth/bluez-5.66.tar.xz
-tar -xf ~/bluez/bluez-*
+tar -xf ~/bluez/bluez-* -C ~/bluez
 rm -r ~/bluez/bluez-*.tar.xz
 cd ~/bluez/bluez-*
 ./configure 
